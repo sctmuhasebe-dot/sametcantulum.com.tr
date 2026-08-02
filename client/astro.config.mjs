fe159833b7d0fe94f20d39ac8edc6675af3f9901
sctmuhasebe-dot <sctmuhasebe@gmail.com>
2026-08-02 04:30:40 +0300

    Tertemiz ilk commit

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,25 +1,12 @@
-// client/astro.config.mjs
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://sametcantulum.com.tr',
+  output: 'static', // Statik moda aldık
   integrations: [sitemap()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
   vite: {
     plugins: [tailwindcss()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:5000',
-          changeOrigin: true,
-        },
-      },
-    },
   },
 });
